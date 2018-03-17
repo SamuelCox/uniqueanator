4418af2301585070bc028f55aff1124cafa8c4d0
samuelcox <sc630@kent.ac.uk>
2018-03-17 19:14:05 +0000

    Initial commit

---
+++ uniqueanator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace uniqueanator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 0)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            var files = Directory.EnumerateFiles(args[0]);
+
+            foreach (var fileName in files)
+            {
+                var extensionStart = fileName.LastIndexOf(".", StringComparison.InvariantCulture);
+                var extension = fileName.Substring(extensionStart, fileName.Length - extensionStart);
+                var guid = Guid.NewGuid();
+                Console.WriteLine($"Moving file {fileName} to File_{guid}{extension}" );
+                File.Move(fileName, $"{args[0]}\\File_{guid}{extension}");
+                Console.WriteLine($"Deleting file {fileName}");
+                File.Delete(fileName);
+                Console.WriteLine($"Finished with file {fileName}");
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
